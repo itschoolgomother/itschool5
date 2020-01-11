13547351c3b12e5a78af762883102f90e8461a14
itschoolgomother <itschoolgomother@gmail.com>
2020-01-11 13:25:04 +0200

    11.1.2020

---
+++ kolia/night.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+canvas.style.backgroundColor = "red";
+
+ctx.fillRect(100,100,100000000000,100000000000);
+
+
+ctx.st
--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import datetime
 import threading
 import os
 
-
-path = "Documents/Andrey"
-
-
 def setInterval(func,time):
     e = threading.Event()
     while not e.wait(time):
         func()
 
-def main():
+
+def main(push,interval,power):
+    print('Push + poweroff = 1 | OnlyPush = 2')
+    info = input()
+    if info == "1":
+        push()
+        interval(power,60)
+    elif info == "2":
+        push()
+
+def off():  
     os.system("poweroff")
        
 def pushGit():
@@ -21,5 +27,6 @@ def pushGit():
     os.system("git commit -m" + data)
     os.system("git push origin master")
 
-pushGit()
-setInterval(main,60)
+main(pushGit,setInterval,off)
+
+
